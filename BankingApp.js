fb3238c8ad9e7eaf82a3e4e372e5083c77e77a06
iandolorito <146801673+iandolorito@users.noreply.github.com>
2025-07-25 19:27:57 +0800

    Create BankingApp.js

---
+++ BankingApp.js
@@ -0,0 +1,41 @@
+function Account(Name, Balance = 0){
+  return{
+    Name: Name,
+    Balance: Balance,
+    Withdrawn: 0,
+    DailyLimit: 10000
+  };
+}
+
+
+function Deposit(account, amount){
+   account.Balance += amount;
+   console.log(account.Name + " Deposit: ₱" + amount.toFixed(2)+ "\nYour new Balance is: ₱"+ account.Balance.toFixed(2) + "\n");
+   return;
+}
+
+function Withdraw(account, amount){
+  
+   if(account.Withdrawn + amount > account.DailyLimit){
+     console.log("Sorry, Daily Withdrawal exceeded\n You can only withdraw exact amount of ₱" + account.DailyLimit.toFixed(2) + "\n");
+     return;
+   }else if(amount > account.Balance){
+     console.log("You don't have enough Balance\n Your current Balance is: ₱" + account.Balance.toFixed(2) + "\n");
+     return;
+   }else{
+     account.Balance -= amount;
+     account.Withdrawn += amount;
+     console.log(account.Name + " Just Successfully Withdrawn amount ₱"+ amount.toFixed(2) + "\nYour remaining balance is: ₱"+account.Balance.toFixed(2) + "\n");
+     return;
+   }
+}
+
+let Ian = Account("Ian", 0);
+let Jayep = Account("Jayep", 0);
+
+
+Deposit(Ian, 5000);
+Withdraw(Ian, 3500);
+
+Deposit(Jayep, 10000);
+Withdraw(Jayep, 5000);
